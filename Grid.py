faf72bdf25637aba4c165257b97c185d2cbf01da
Anthony Jinete <32381155+anthonyjinete45@users.noreply.github.com>
2017-12-03 21:32:08 -0500

    Add files via upload

---
+++ Grid.py
@@ -0,0 +1,48 @@
+
+#Constructs grid object of certain dimensions depending on number of cards
+class Grid:
+
+    def __init__(self, num_cards, card_list):
+
+        self.cards=num_cards
+
+
+        if num_cards==20:
+            self.x=5
+            self.y=4
+        elif num_cards==40:
+            self.x=8
+            self.y=5
+        elif num_cards==60:
+            self.x=6
+            self.y=10
+
+        grid_list=[]
+        for i in range(self.y):
+            grid_list.append([])
+            #accessed wiht grid_list[r][c]
+            #length of grid_list {self.y} is the number of rows in grid
+            #length of grid_list's internal lists {self.x} is num of columns
+
+        #if num_cards==20:
+            
+        if num_cards==40:
+
+            i=0
+            #Adds cards in card_list to 2D grid_list
+            for r in range(self.y):
+                for c in range(self.x):
+                    grid_list[r][c]=card_list[i]
+                    i+=1
+                
+            
+        #if num_cards==60:
+            
+
+
+class GridSquare:
+
+    def __init__(self, xcoor, ycoor, occupy_state):
+
+        self.x=xcoor
+        self.y=ycoor
