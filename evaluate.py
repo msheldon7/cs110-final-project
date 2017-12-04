2ac45991682adbdb623976694f72a0874e3cf044
Anthony Jinete <32381155+anthonyjinete45@users.noreply.github.com>
2017-12-03 19:55:18 -0500

    Update evaluate.py
    Deleted game.py, transferred its code to evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,20 @@
 
 #Will compare front image file names to see if they are the same
-def isMatching(self, card1, card2):
+def isMatching(card1, card2):
 
-        if(card1.img==card2.img):
+        if(card1.front==card2.front):
             return True
 
         else:
             return False
+
+
+#If game is finished - will see if all cards have been revealed
+def isFinished(cardList):
+
+    for card in cardList:
+
+        if card.revealed==False:
+            return False
+
+    return True
