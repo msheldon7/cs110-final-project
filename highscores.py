f9291ce1eba6ad485fb02afa69c0f0f62ccd6032
kmoshie1 <33292245+kmoshie1@users.noreply.github.com>
2017-12-04 20:36:01 -0500

    Add files via upload

---
+++ highscores.py
@@ -0,0 +1,42 @@
+import pygame
+import findButton
+import textInput
+import sys
+import Button
+
+def highscores():
+    black = (0,0,0)
+    white = (255,255,255)
+
+    display_width = 800
+    display_length = 600
+
+    gameDisplay = pygame.display.set_mode((display_width, display_length))
+    gameDisplay.fill(white)
+
+    font1 = pygame.font.SysFont("Times New Roman", 50)
+    title = font1.render("HIGHSCORES", True, black)
+    gameDisplay.blit(title, (205, 10))
+
+    font2 = pygame.font.SysFont("Arial", 30)
+    name = font2.render("Name:", True, black)
+    gameDisplay.blit(name, (60, 120))
+    gameDisplay.blit(name, (460, 120))
+    score = font2.render("Score:", True, black)
+    gameDisplay.blit(score, (280, 120))
+    gameDisplay.blit(score, (680, 120))
+
+    back = Button.Button(gameDisplay, white, green, red, (260, 540), (255, 50), "Menu")
+    pygame.display.update()
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            if findButton(back):
+                running = False
+
+            else:
+                running = True
---
+++ textInput.py
@@ -0,0 +1,116 @@
+import pygame
+import Button
+
+def textInput(screen, base):
+    person = ""
+    inputString = []
+    time = 0
+    enter = True
+
+    while enter:
+        person = ""
+        preLength = len(inputString)
+        for event in pygame.event.get()
+            if event.type == pygame.KEYDOWN
+                time = 90
+                if event.key == pygame.K_0:
+                    inputString.append("0")
+                elif event.key == pygame.K_1:
+                    inputString.append("1")
+                elif event.key == pygame.K_2:
+                    inputString.append("2")
+                elif event.key == pygame.K_3:
+                    inputString.append("3")
+                elif event.key == pygame.K_4:
+                    inputString.append("4")
+                elif event.key == pygame.K_5:
+                    inputString.append("5")
+                elif event.key == pygame.K_6:
+                    inputString.append("6")
+                elif event.key == pygame.K_7:
+                    inputString.append("7")
+                elif event.key == pygame.K_8:
+                    inputString.append("8")
+                elif event.key == pygame.K_9:
+                    inputString.append("9")
+                elif event.key == pygame.K_q:
+                    inputString.append("q")
+                elif event.key == pygame.K_w:
+                    inputString.append("w")
+                elif event.key == pygame.K_e:
+                    inputString.append("e")
+                elif event.key == pygame.K_r:
+                    inputString.append("r")
+                elif event.key == pygame.K_t:
+                    inputString.append("t")
+                elif event.key == pygame.K_y:
+                    inputString.append("y")
+                elif event.key == pygame.K_u:
+                    inputString.append("u")
+                elif event.key == pygame.K_i:
+                    inputString.append("i")
+                elif event.key == pygame.K_o:
+                    inputString.append("o")
+                elif event.key == pygame.K_p:
+                    inputString.append("p")
+                elif event.key == pygame.K_a:
+                    inputString.append("a")
+                elif event.key == pygame.K_s:
+                    inputString.append("s")
+                elif event.key == pygame.K_d:
+                    inputString.append("d")
+                elif event.key == pygame.K_f:
+                    inputString.append("f")
+                elif event.key == pygame.K_g:
+                    inputString.append("g")
+                elif event.key == pygame.K_h:
+                    inputString.append("h")
+                elif event.key == pygame.K_j:
+                    inputString.append("j")
+                elif event.key == pygame.K_k:
+                    inputString.append("k")
+                elif event.key == pygame.K_l:
+                    inputString.append("l")
+                elif event.key == pygame.K_z:
+                    inputString.append("z")
+                elif event.key == pygame.K_x:
+                    inputString.append("x")
+                elif event.key == pygame.K_c:
+                    inputString.append("c")
+                elif event.key == pygame.K_v:
+                    inputString.append("v")
+                elif event.key == pygame.K_b:
+                    inputString.append("b")
+                elif event.key == pygame.K_n:
+                    inputString.append("n")
+                elif event.key == pygame.K_m:
+                    inputString.append("m")
+                elif event.key == pygame.K_RETURN:
+                    enter = False
+                elif event.key == pygame.K_BACKSPACE:
+                    if len(inputString)>0:
+                        base.drawButtonCover()
+                        del inputString[-1]
+                else:
+                    time = 0
+            pygame.event.clear()
+        if preLength != len(inputString):
+input
+
+rollover
+
+        if len(inputString) > 8
+            num = 7
+            while num<50:
+                try:
+                    del inputString[num]
+                    num += 1
+                except:
+                    num = 50
+        for element in inputString:
+            time += element
+        base.setText(person)
+    base.drawButton()
+    pygame.time.wait(time)
+
+return base
