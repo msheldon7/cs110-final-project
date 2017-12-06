7ead57c9c0abdf38475ee7b176e6b07a8e747d7e
msheldon7 <m.sheldon7@yahoo.com>
2017-12-06 12:56:24 -0500

    Readded code
    Inadvertently removed code in my last push, adding it back in here

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import highscores
 import sys
 import Button
 import findButton
+import Game
 
 def menu():
     '''
@@ -10,19 +11,22 @@ def menu():
     param list: none
     returns: None
     '''
+    white=(255,255,255)
+    red=(255,0,0)
+    green=(0,255,0)
     screen = "menu"
     while screen == "menu":
-        title = pygame.image.load("")
-        gameDisplay = pygame.display.set_mode((800,600))
+        title = pygame.image.load("title.png")
+        gameDisplay = pygame.display.set_mode((970,650))
         gameDisplay.fill("white")
         pygame.display.set_caption("Winter Card Matcher")
-        gameDisplay.blit(title,(245,5))
+        gameDisplay.blit(title,(150,0))
         running = True
-        play = Button.Button(gameDisplay, white, green, red, (200, 100), (150, 50), "START")
+        play = Button.Button(gameDisplay, white, green, red, (200, 200), (150, 50), "START")
 
-        highscore = Button.Button(gameDisplay, white, green, red, (200, 200), (250, 50), "HIGHSCORES")
+        highscore = Button.Button(gameDisplay, white, green, red, (200, 300), (250, 50), "HIGHSCORES")
 
-        quitGame = Button.Button(gameDisplay, white, green, red, (200, 300), (350, 50), "QUIT")
+        quitGame = Button.Button(gameDisplay, white, green, red, (200, 300), (400, 50), "QUIT")
 
         while running:
             for event in pygame.event.get():
@@ -30,16 +34,16 @@ def menu():
                     pygame.quit()
                     sys.exit()
 
-                if findButton(quitGame):
+                if findButton.buttonDetect(quitGame):
                     running = False
                     pygame.quit()
                     quit()
 
-                elif findButton(play):
+                elif findButton.buttonDetect(play):
                     running = False
-                    mode.mode(amountOfCards)
+                    Game.game(24)
 
-                elif findButton(highscore):
+                elif findButton.buttonDetect(highscore):
                     running = False
                     highscore.highscores()
 
