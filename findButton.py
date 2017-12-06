7c43e471806ed74f2fdcbbacebfcd9bec492dad8
Anthony Jinete <32381155+anthonyjinete45@users.noreply.github.com>
2017-12-06 11:49:49 -0500

    Fix syntax errors

--- findButton.py
+++ findButton.py
@@ -1,12 +1,13 @@
+
 import pygame
 
 def buttonDetect(button):
     pressed = False
     x, y = pygame.mouse.get_pos()
-    buttonXOne = button.getRectCords()[0]
-    buttonXTwo = buttonXOne + button.getRectSize()[0]
-    buttonYOne = button.getRectCords()[1]
-    buttonYTwo = buttonYOne + button.getRectSize()[1]
+    buttonXOne = button.getCoordinates()[0]
+    buttonXTwo = buttonXOne + button.getsize()[0]
+    buttonYOne = button.getCoordinates()[1]
+    buttonYTwo = buttonYOne + button.getsize()[1]
 
     if x>buttonXOne and x<buttonXTwo and y>buttonYOne and y<buttonYTwo:
         button.drawButtonActive()
