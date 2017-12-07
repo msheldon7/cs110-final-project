8ba2e85271bc46e239dfcfb7b93e787aedad8e56
kmoshie1 <33292245+kmoshie1@users.noreply.github.com>
2017-12-06 19:29:51 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,6 +1,63 @@
 import pygame
 import highscores
+import sysimport pygame
+import highscores
 import sys
+import Button
+import findButton
+import Game
+
+def menu():
+    '''
+    This function sets up the display for the main menu and has some logic determining whether or not the menu should remain on screen.
+    param list: none
+    returns: None
+    '''
+    white=(255,255,255)
+    red=(255,0,0)
+    green=(0,255,0)
+    screen = "menu"
+    brown = (139, 69, 19)
+    while screen == "menu":
+        title = pygame.image.load("title.png")
+        present = pygame.image.load("present.png")
+        gameDisplay = pygame.display.set_mode((970,650))
+        gameDisplay.fill(red)
+        pygame.display.set_caption("Winter Card Matcher")
+        gameDisplay.blit(title,(100,0))
+        gameDisplay.blit(present,(600, 300))
+        running = True
+        play = Button.Button(gameDisplay, white, green, white, (200, 230), (175, 50), "   START")
+
+        stump = Button.Button(gameDisplay, white, brown, brown, (250, 480),(50, 50), "")
+
+        highscore = Button.Button(gameDisplay, white, green, white, (160, 330), (250, 50), " HIGHSCORES")
+
+        quitGame = Button.Button(gameDisplay, white, green, white, (120, 430), (325, 50), "             QUIT")
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if findButton.buttonDetect(quitGame):
+                    running = False
+                    pygame.quit()
+                    quit()
+
+                elif findButton.buttonDetect(play):
+                    running = False
+                    Game.game(24)
+
+                elif findButton.buttonDetect(highscore):
+                    running = False
+                    highscore.highscores()
+
+                else:
+                    running = True
+menu()
+
 import Button
 import findButton
 import Game
