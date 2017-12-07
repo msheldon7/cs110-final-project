2a4e3c1d4c327ca0894f643587e233d476cca547
Anthony Jinete <32381155+anthonyjinete45@users.noreply.github.com>
2017-12-06 20:10:19 -0500

    Update imageCheck.py

--- imageCheck.py
+++ imageCheck.py
@@ -1,5 +1,5 @@
 def imageCheck(card1,card2,points):
-    if card1.getCardImage() != card2.getCardImage():
+    if card1.getFront_image_file() != card2.getFront_image_file():
         card1.setClicked(False)
         card2.setClicked(False)
         points = 0
