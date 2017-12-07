2102d102510df123bcf1b648316014e7e44ff0a5
msheldon7 <m.sheldon7@yahoo.com>
2017-12-06 23:45:29 -0500

    Update imageCheck.py

--- imageCheck.py
+++ imageCheck.py
@@ -1,4 +1,9 @@
 def imageCheck(card1,card2,points):
+    '''
+    This function checks  if the cards are matching or not and returns the outcome.
+    param list: card1, card2, points
+    returns: array of card1, card2, and points
+    '''
     if card1.getFront_image_file() != card2.getFront_image_file():
         card1.setClicked(False)
         card2.setClicked(False)
